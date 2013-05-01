9bd08960921ca73eb45a6030f0989942d27149ff
dazzyd <dazzyd2@gmail.com>
2013-05-01 15:11:44 +0800

    Add home.py

--- tweetcntd/urls.py
+++ tweetcntd/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tweetcntd.views.home', name='home'),
+    url(r'^$', 'tweetcntd.views.home.main', name='home'),
     url(r'^hello/$', 'tweetcntd.views.hello.main'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ tweetcntd/views/home.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from tweetcntd.models import config
+
+HOME = '''
+<html>
+	<head><title>tweetcntd</title></head>
+	<body>
+		<h2>tweetcntd #VERSION#</h2>
+		<p>Non-Public, but <a href="https://github.com/dazzyd/tweetcntd">Open Source</a>.</p>
+		<p><i>统计时段：-0:30~23:30、推数 #TWEET_MIN# 以下不发统计<br/></i></p>
+	</body>
+</html>
+'''\
+.replace("#VERSION#", config.VERSION)\
+.replace("#TWEET_MIN#", str(config.TWEET_MIN))
+
+def main(request):
+	return HttpResponse(HOME)
