328e57e312d51f744743b0c8fa09859602197de5
dazzyd <h@dazzyd.org>
2013-05-09 20:52:16 +0800

    Database config, Update comments.

--- tweetcntd/config.py
+++ tweetcntd/config.py
@@ -1,14 +1,28 @@
-
+''' Application
+ No need to edit.
+'''
 VERSION = 'ver 0.0.1'
 CONSUMER_KEY = 'CjUSfZ1IDHrBe9dLu5Viyw'
 CONSUMER_SECRET = '3iUPswMl97gxxirCK5rVN3MvNqM5AcB1dTnxlmdMyQ'
 
+''' Server
+ (....)
+'''
 HOST = 'http://127.0.0.1:8000'
 AUTH_KEY = '0x0'
 TWEET_MIN = 8
 
+''' Databse
+ (....)
+'''
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 3306
+DATABASE_TABLE = 'tweetcntd'
+DATABASE_USERNAME = 'tweetcntd'
+DATABASE_PASSWORD = 'i&X6nHFQc^FExN17'
+
 ''' Admin Tools
- Change ADMIN_KEY when you set ADMIN enable.
+ Don't forget to change ADMIN_KEY if want to use Admin Tools.
 '''
 ADMIN_ENABLED = False
 ADMIN_KEY = '96Apt&#Az9xPU*Jq'
