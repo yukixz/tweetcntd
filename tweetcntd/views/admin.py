060366da948c3391ca4ccf59d5013e860acf900b
dazzyd <h@dazzyd.org>
2013-05-11 18:31:08 +0800

    Ignore it

--- tweetcntd/views/admin.py
+++ tweetcntd/views/admin.py
@@ -1,9 +1,11 @@
-
+from django.conf.urls import patterns, url
 from tweetcntd import config
 
 def list(request):
 	pass
 
-urlpatterns = patterns('tweetcntd.views',
-    url(r'^list/$', 'admin.list'),
+
+if config.ADMIN_ENABLED:
+	urlpatterns = patterns('tweetcntd.views',
+	url(r'^list/$', 'admin.list'),
 )
