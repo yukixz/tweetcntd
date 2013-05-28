31246fce459e9786f32a6eb172e44705c3e021fa
Dazzy Ding <h@dazzyd.org>
2013-05-29 00:23:00 +0800

    Bug Fixed. Ignore it.

--- tweetcntd/views/utils.py
+++ tweetcntd/views/utils.py
@@ -6,6 +6,7 @@ def Redirect2HomePage():
 	response = HttpResponse(status=302)
 	response['Location'] = config.HOST
 	response.content = Templates.HTML_REDIRECT.replace("{{url}}", config.HOST)
+	return response
 
 def Redirect2URL(url=''):
 	if not url:
@@ -13,3 +14,4 @@ def Redirect2URL(url=''):
 	response = HttpResponse(status=302)
 	response['Location'] = url
 	response.content = Templates.HTML_REDIRECT.replace("{{url}}", url)
+	return response
