4dae585313b5d10d70f7d86c6224e4efd247dbb7
Dazzy Ding <h@dazzyd.org>
2013-06-15 17:13:11 +0800

    Add Exception Handling of Twitter, Running backends.post as an Object.

--- tweetcntd/backends/post.py
+++ tweetcntd/backends/post.py
@@ -6,74 +6,86 @@ from datetime import datetime, timedelta
 import re
 from tweetcntd import config
 from tweetcntd.models.database import Database
-from tweetcntd.models.twitter import TwitterClient
-from tweetcntd.models.twitter import TwitterUser
+from tweetcntd.models.twitter import TwitterClient, TwitterError, TwitterUser
 from tweetcntd.views import Templates
 
-def post():
-    database = Database(config.DATABASE_HOST, config.DATABASE_PORT,
-        config.DATABASE_DATABASE, config.DATABASE_TABLE, config.DATABASE_ISINNODB,
-        config.DATABASE_USERNAME, config.DATABASE_PASSWORD)
-    client = TwitterClient(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+class Post():
+    def __init__(self):
+        self.database = Database(config.DATABASE_HOST, config.DATABASE_PORT,
+            config.DATABASE_DATABASE, config.DATABASE_TABLE, config.DATABASE_ISINNODB,
+            config.DATABASE_USERNAME, config.DATABASE_PASSWORD)
+        self.client = TwitterClient(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+        
+        self.PATTERN_RE=re.compile(r'^(@\w+)\b.*$')
+        self.PATTERN_RT=re.compile(r'^.*?(RT ?@\w+)\b.*$')
+        self.MONTH2NUMBER={'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+        
+        ''' Init formatted start_time and end_time. '''
+        delta = timedelta(hours=config.TIMEZONE)
+        now_tz = datetime.utcnow() + delta
+        post_str = now_tz.strftime('%Y-%m-%d ') + config.POST_TIME
+        post_utc = datetime.strptime(post_str, '%Y-%m-%d %H:%M') - delta
+        self.end_time = post_utc.strftime("%Y%m%d%H%M%S")
+        self.start_time = (post_utc-timedelta(hours=24)).strftime("%Y%m%d%H%M%S")
+        
+    def run(self):
+        li = self.database.query_all()
+        for user in li:
+            if not user.enabled: continue
+            try:
+                oauth_user = TwitterUser(user.token, user.secret)
+                (sum, re, rt, rto) = self.count_user(oauth_user)
+                
+                if sum>config.TWEET_MIN and sum>0:
+                    status = Templates.TWITTER_TWEET.replace('{{name}}', user.name) % \
+                        ( sum, re, float(re)/sum*100 , rt, float(rt)/sum*100, rto, float(rto)/sum*100 )
+                    print(status) #self.client.tweet(oauth_user, status)
+                
+            except TwitterError as e:
+                if e.code==1: continue
+                if e.code==2: break
+                if e.code==3: self.database.disable_user(user.id)
+                if e.code==4: continue  # How to Retry?
+                if e.code==5: break
     
-    start_time, end_time = get_time()
-    li = database.query_all()
-    for user in li:
-        oauth_user = TwitterUser(user.token, user.secret)
-        (sum, re, rt, rto) = count_user(client, oauth_user, start_time, end_time)
-        if sum>config.TWEET_MIN and sum>0:
-            status = Templates.TWITTER_TWEET.replace('{{name}}', user.name) % \
-                ( sum, re, float(re)/sum*100 , rt, float(rt)/sum*100, rto, float(rto)/sum*100 )
-            client.tweet(oauth_user, status)
+    def count_user(self, user):
+        # init
+        max_id = 0
+        timeline = []
+        block = [{"created_at":"Tue Feb 14 00:00:00 +0000 9999"}]    ## Magic
+        
+        # Generate user's new tweets' blocks
+        while self.format_time(block[len(block)-1]["created_at"]) > self.start_time:
+            block = self.client.load_usrtl(user, max_id)
+            timeline.extend(block)
+            max_id = block[len(block)-1]["id"]
+        
+        # Count user's tweets
+        (sum, re, rt, rto) = (0,0,0,0)
+        for tweet in timeline:
+            tweet_time = self.format_time(tweet["created_at"])
+            if tweet_time > self.end_time:
+                continue
+            elif tweet_time > self.start_time:
+                sum +=1
+                if tweet.get('retweeted_status', False):
+                    rto += 1
+                elif self.PATTERN_RE.match(tweet["text"]):
+                    re += 1
+                elif self.PATTERN_RT.match(tweet["text"]):
+                    rt +=1
+            else:
+                break # for
+        
+        return sum, re, rt, rto
     
-    database.close()
-
-def count_user(client, user, start_time, end_time,
-        PATTERN_RE=re.compile(r'^(@\w+)\b.*$'), PATTERN_RT=re.compile(r'^.*?(RT ?@\w+)\b.*$')):
-    # init
-    max_id = 0
-    timeline = []
-    block = [{"created_at":"Tue Feb 14 00:00:00 +0000 9999"}]    ## Magic
-    
-    # Generate user's new tweets' blocks
-    while format_time(block[len(block)-1]["created_at"]) > start_time:
-        block = client.load_usrtl(user, max_id)
-        timeline.extend(block)
-        max_id = block[len(block)-1]["id"]
-    
-    # Count user's tweets
-    (sum, re, rt, rto) = (0,0,0,0)
-    for tweet in timeline:
-        tweet_time = format_time(tweet["created_at"])
-        if tweet_time > end_time:
-            continue
-        elif tweet_time > start_time:
-            sum +=1
-            if tweet.get('retweeted_status', False):
-                rto += 1
-            elif PATTERN_RE.match(tweet["text"]):
-                re += 1
-            elif PATTERN_RT.match(tweet["text"]):
-                rt +=1
-        else:
-            break # for
-    
-    # return
-    return sum, re, rt, rto
-
-def format_time(ss, MONTH2NUMBER={'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',
-        'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}):
-    return ''.join(( ss[26:30],MONTH2NUMBER[ss[4:7]],ss[8:10],ss[11:13],ss[14:16],ss[17:19] ))
-
-def get_time():
-    ''' Returns formatted start_time and end_time. '''
-    delta = timedelta(hours=config.TIMEZONE)
-    now_tz = datetime.utcnow() + delta
-    post_str = now_tz.strftime('%Y-%m-%d ') + config.POST_TIME
-    post_utc = datetime.strptime(post_str, '%Y-%m-%d %H:%M') - delta
-    end = post_utc.strftime("%Y%m%d%H%M%S")
-    start = (post_utc-timedelta(hours=24)).strftime("%Y%m%d%H%M%S")
-    return start, end
+    def format_time(self, ss):
+        return ''.join(( ss[26:30], self.MONTH2NUMBER[ss[4:7]],ss[8:10],ss[11:13],ss[14:16],ss[17:19] ))
+        
+    def close(self):
+        self.database.close()
 
 if __name__ == '__main__':
-    post()
+    app = Post()
+    app.run()
+    app.close()
--- tweetcntd/models/twitter.py
+++ tweetcntd/models/twitter.py
@@ -7,9 +7,18 @@ class TwitterUser():
         self.access_secret = secret
     
 
-class RequestError(Exception):
-    def __init__(self, r):
-        self.request = r
+ERROR_MESSAGE = {
+    0: 'Anything is OK.',
+    1: 'Skip the User.',
+    2: 'Skip the Rest User.',
+    3: 'Disable the User.',
+    4: 'Retry.',
+    5: 'Critical!',
+}
+class TwitterError(Exception):
+    def __init__(self, code=0):
+        self.code = code
+        self.message = ERROR_MESSAGE.get(code, 'Invalid Error Code.')
     
 
 class TwitterClient():
@@ -20,15 +29,72 @@ class TwitterClient():
             callback_url = callback_url
         )
     
+    
     def request(self, method, url, params, token, secret):
         r = self.client.request(method, url, params, token, secret)
-        if r.status_code==200: return r
-        else: raise RequestError(r)
+        
+        status_code = r.status_code
+        r_content = r.json()
+        try:    error_code = ['errors'][0]['code']
+        except: error_code = 0
+        
+        # https://dev.twitter.com/docs/error-codes-responses
+        if status_code==200:    # 200 OK
+            return r
+        if status_code==304:    # 304 Not Modified
+            return r
+        if status_code==400:    # 400 Bad Requests
+            pass # Below
+        if status_code==401:    # 401 Unauthorized
+            pass # Below
+        if status_code==403:    # 403 Forbidden
+            pass # Below
+        if status_code==404:    # 404 Not Found
+            pass # Below
+        if status_code==406:    # 406 Not Acceptable
+            pass
+        if status_code==408:    #### 408 Request Timeout
+            raise TwitterError(4)
+        if status_code==410:    # 410 Gone
+            pass
+        if status_code==422:    # 422 Unprocessable Entity
+            pass
+        if status_code==429:    # 429 Too Many Requests
+            raise TwitterError(1)
+        if status_code==500:    # 500 Internal Server Error
+            raise TwitterError(2)
+        if status_code==502:    # 502 Bad Gateway
+            raise TwitterError(2)
+        if status_code==503:    # 503 Service Unavailable
+            raise TwitterError(2)
+        if status_code==504:    # 504 Gateway timeout
+            raise TwitterError(2)
+        
+        ## Error Code corresponded with HTTP Status Code above are ignored.
+        if error_code==32:  # 32 Could not authenticate you
+            raise TwitterError(3)
+        if error_code==34:  # 34 Sorry, that page does not exist
+            raise TwitterError(3)
+        if error_code==64:  # 64 Your account is suspended and is not permitted to access this feature
+            raise TwitterError(3)
+        #if error_code==88: # 88 Rate limit exceeded
+        if error_code==89:  # 89 Invalid or expired token
+            raise TwitterError(3)
+        #if error_code==130:# 130 Over capacity
+        #if error_code==131:# 131 Internal error
+        if error_code==135: # 135 Could not authenticate you
+            raise TwitterError(1)
+        if error_code==187: # 187 Status is a duplicate
+            raise TwitterError(1)
+        if error_code==215: # 215 Bad authentication data
+            raise TwitterError(2)
+        
+        return r
     
-    def get (url, params={}, token='', secret=''):
+    def get (self, url, params={}, token='', secret=''):
         return self.request('GET', url, params, token, secret)
     
-    def post(url, params={}, token='', secret=''):
+    def post(self, url, params={}, token='', secret=''):
         return self.request('POST', url, params, token, secret)
     
     
@@ -50,7 +116,7 @@ class TwitterClient():
         return int(result["user_id"][0]), result["screen_name"][0], result["oauth_token"][0], result["oauth_token_secret"][0]
     
     
-    def load_usrtl(self, user, max_id=0, count=200):
+    def load_usrtl(self, user, max_id=0, count=100):
         url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
         params = {'trim_user':1}
         # if since_id: params['since_id'] = since_id
