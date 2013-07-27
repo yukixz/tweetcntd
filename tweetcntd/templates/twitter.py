bc364e5d83a5151db3f7f858aca6dd9704a3e047
Dazzy Ding <h@dazzyd.org>
2013-07-27 13:36:13 +0800

    New tweet template.

--- tweetcntd/templates/twitter.py
+++ tweetcntd/templates/twitter.py
@@ -1,6 +1,6 @@
 from tweetcntd import config
 
-TWITTER_TWEET = "@{{name}} 本日共发 %d 推，其中 @ %d 推（%.1f%%）、RT @ %d 推（%.1f%%）、Retweet %d 推（%.1f%%）"
+TWITTER_TWEET = "@{{name}} 本日共发 %d 推，其中回复 %d (%.1f%%)、引用 %d (%.1f%%)、转发 %d (%.1f%%)"
 def tweet(name, sum, re, rt, rto):
     status = TWITTER_TWEET.replace('{{name}}', name)
     status = status % ( sum, re, float(re)/sum*100 , rt, float(rt)/sum*100, rto, float(rto)/sum*100 )
