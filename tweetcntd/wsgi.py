452e1a21ae2f4be4086a40166c78373af8d7fa5e
Dazzy Ding <h@dazzyd.org>
2013-09-09 22:05:24 +0800

    Prepare for including packages

--- tweetcntd/wsgi.py
+++ tweetcntd/wsgi.py
@@ -15,6 +15,10 @@ framework.
 """
 import os
 
+# Custom setting #
+import sys
+sys.path.insert(0, "./tweetcntd/packages/")
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
